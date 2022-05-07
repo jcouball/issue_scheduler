0b5225b1246c96559312aacff3a01e0c4c3af0a9
James Couball <couballj@yahooinc.com>
2022-05-06 17:39:23 -0700

    Add lib_dir method

--- lib/issue_scheduler.rb
+++ lib/issue_scheduler.rb
@@ -6,4 +6,8 @@ require_relative 'issue_scheduler/version'
 
 # A module to encapsulate the Issue Scheduler functionality
 # @api puiblic
-module IssueScheduler; end
+module IssueScheduler
+  def self.lib_dir
+    File.join(__dir__, 'issue_scheduler')
+  end
+end
--- spec/issue_scheduler_spec.rb
+++ spec/issue_scheduler_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe IssueScheduler do
   it 'has a version number' do
     expect(IssueScheduler::VERSION).not_to be nil
   end
+
+  describe '.lib_dir' do
+    subject { IssueScheduler.lib_dir }
+    it { is_expected.to be_a(String) }
+  end
 end
